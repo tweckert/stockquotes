4242f051554ea9f6f9c2cca18439c2594610af96
Thomas Weckert <thomas.weckert@neofonie.de>
2023-04-18 08:28:25 +0200

    Import filters duplicate elements.

--- src/main/java/javax/finance/stockquotes/persistence/entity/Stock.java
+++ src/main/java/javax/finance/stockquotes/persistence/entity/Stock.java
@@ -1,5 +1,7 @@
 package javax.finance.stockquotes.persistence.entity;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -67,6 +69,38 @@ public class Stock {
                 .append("wkn", wkn)
                 .append("name", name)
                 .toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(name)
+                .append(isin)
+                .append(wkn)
+                .toHashCode();
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj == this) {
+            return true;
+        }
+
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+
+        final Stock otherStock = (Stock) obj;
+        return new EqualsBuilder()
+                .append(name, otherStock.getName())
+                .append(wkn, otherStock.getWkn())
+                .append(isin, otherStock.getIsin())
+                .isEquals();
     }
 
 }
--- src/main/java/javax/finance/stockquotes/service/impl/AbstractImportService.java
+++ src/main/java/javax/finance/stockquotes/service/impl/AbstractImportService.java
@@ -13,8 +13,7 @@ import javax.finance.stockquotes.persistence.entity.StockQuote;
 import javax.finance.stockquotes.persistence.repository.StockQuoteRepository;
 import javax.finance.stockquotes.service.ImportService;
 import javax.finance.stockquotes.yahoo.service.YahooImportService;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public abstract class AbstractImportService implements ImportService {
 
@@ -37,6 +36,23 @@ public abstract class AbstractImportService implements ImportService {
         // sort oldest to youngest
         importStockQuoteList.sort(Comparator.comparing(StockQuote::getDate));
 
+        // remove duplicate elements
+        final Set<StockQuote> stockQuoteSet = new HashSet<>();
+        final Iterator<StockQuote> stockQuoteIterator = importStockQuoteList.iterator();
+        while (stockQuoteIterator.hasNext()) {
+
+            final StockQuote stockQuote = stockQuoteIterator.next();
+            if (!stockQuoteSet.contains(stockQuote)) {
+                stockQuoteSet.add(stockQuote);
+            } else {
+
+                stockQuoteIterator.remove();
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug(StringUtils.join("Removed duplicate stock quote ", stockQuote));
+                }
+            }
+        }
+
         final StockQuote oldestStockQuote = importStockQuoteList.get(0);
         final StockQuote youngestStockQuote = importStockQuoteList.get(importStockQuoteList.size() - 1);
 
@@ -55,7 +71,7 @@ public abstract class AbstractImportService implements ImportService {
 
     @Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
     protected void transactionalUpdateStockQuotes(final List<StockQuote> removeStockQuoteList,
-                                                  final List<StockQuote> importStockQuoteList) {
+                                               final List<StockQuote> importStockQuoteList) {
         stockQuoteRepository.deleteAll(removeStockQuoteList);
         stockQuoteRepository.saveAll(importStockQuoteList);
     }
